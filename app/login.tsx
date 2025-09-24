3e91f678899f92e94111b12be04f6ad3e6b46259
juanCaceres <jcruz2013c@gmail.com>
2025-09-24 11:20:19 -0300

    comente las funciones del login

--- app/login.tsx
+++ app/login.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Text, StyleSheet, View, TextInput, TouchableOpacity, Image } from "react-native";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
+
+
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../App";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
@@ -10,12 +12,15 @@ type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, "Login"
 type Props = { navigation: LoginScreenNavigationProp };
 
 export default function Login({ navigation }: Props) {
+  //declaracion de estados
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorEmail, setErrorEmail] = useState("");
   const [errorPassword, setErrorPassword] = useState("");
 
-  const validarCampos = () => {
+  //FUNCIONES
+
+  const validarCampos = () => { //funcion para validar campos
     let valid = true;
     setErrorEmail("");
     setErrorPassword("");
@@ -31,7 +36,7 @@ export default function Login({ navigation }: Props) {
     return valid;
   };
 
-  const logueo = async () => {
+  const logueo = async () => { //funcion para loguear
     if (!validarCampos()) return;
     try {
       await signInWithEmailAndPassword(auth, email, password);
@@ -44,6 +49,9 @@ export default function Login({ navigation }: Props) {
       }
     }
   };
+
+
+  
 
   return (
     <View style={styles.container}>
